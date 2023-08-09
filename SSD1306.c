fae97aeca9d4b754bd7edd9a55b60bcf3409e4b9
madmoody <73907801+madmoody@users.noreply.github.com>
2023-08-09 14:58:07 +0530

    Update SSD1306.c

--- SSD1306.c
+++ SSD1306.c
@@ -4,54 +4,52 @@
 #include <linux/i2c-dev.h>
 #include <sys/ioctl.h>
 #include <unistd.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
 
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
 #define OLEDADD 0x3C
 #define PATH "/dev/i2c-2"
 int fd;
 
-void OLED_Init(){
-char OledWrite[1];
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
 
-OledWrite[0] = 0xAE;
-OledWrite[1] = 0xAF;
+void OLED_Init() {
+    display.begin(SSD1306_I2C_ADDRESS, OLEDADD);
+    display.display();
+    delay(2000); // Delay for initialization
 
-write(fd,OledWrite,1);
-
+    display.clearDisplay();
 }
 
-void I2C_Init(){
-fd = open(PATH,O_RDWR);
-if(fd < 0){
-printf(": Path is not open : ");
-return 1;
-}
-if(ioctl(fd,I2C_SLAVE,OLEDADD<0){
-printf(" : Failed to Set OLED : ");
-return 1;	
-}
-}
-
-void Oled_display(char Character){
-char OledWrite[1];
-OledWrite[0] = 0x00;
-OledWrite[1] = character;
-
-write(fd,OledWrite,1);
+void I2C_Init() {
+    fd = open(PATH, O_RDWR);
+    if (fd < 0) {
+        printf(": Path is not open : ");
+        exit(1);
+    }
+    if (ioctl(fd, I2C_SLAVE, OLEDADD) < 0) {
+        printf(" : Failed to Set OLED : ");
+        exit(1);
+    }
 }
 
-
-void OLED_Print(char const *str){
-while(*str){
-Oled_display(*str);
-str++;	
+void OLED_Print(const char *str) {
+    display.setTextSize(1);      // Set text size
+    display.setTextColor(SSD1306_WHITE); // Set text color
+    display.setCursor(0, 0);     // Set text position
+    display.println(str);        // Print text
+    display.display();
 }
 
-}
+int main() {
+    I2C_Init();
+    OLED_Init();
+    OLED_Print("Hello World");
+    while (1) {
+        // Your main application code here
+    }
 
-int main(){
-I2C_Init();
-OLED_Init();
-OLED_Print("Hello World");
-while(1);
-
+    return 0;
 }
